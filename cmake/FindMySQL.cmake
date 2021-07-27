e50cefaddf13b6be16ee8abc3b9b3e7ea4978e94
Daniil Kovalev <kovdan01@yandex.ru>
2021-07-27 09:57:43 +0300

    Various fixes

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -5,26 +5,26 @@ if(DEFINED MSVC)
         find_path(MySQL_INCLUDE_DIR
             NAMES mysql.h
             PATHS "$ENV{ProgramFiles}/mariadb-connector-c/include/mariadb"
-                  "$ENV{ProgramFiles(x86)}/mariadb-connector-c/include/mariadb"
+                  "$ENV{ProgramFiles\(x86\)}/mariadb-connector-c/include/mariadb"
             PATH_SUFFIXES include
         )
         find_library(MySQL_LIBRARY
             NAMES libmariadb
             PATHS "$ENV{ProgramFiles}/mariadb-connector-c/lib/mariadb"
-                  "$ENV{ProgramFiles(x86)}/mariadb-connector-c/lib/mariadb"
+                  "$ENV{ProgramFiles\(x86\)}/mariadb-connector-c/lib/mariadb"
             PATH_SUFFIXES lib
         )
     else()
         find_path(MySQL_INCLUDE_DIR
             NAMES mysql_version.h
             PATHS "$ENV{ProgramFiles}/MySQL/MySQL Server 8.0"
-                  "$ENV{ProgramFiles(x86)}/MySQL/MySQL Server 8.0"
+                  "$ENV{ProgramFiles\(x86\)}/MySQL/MySQL Server 8.0"
             PATH_SUFFIXES include
         )
         find_library(MySQL_LIBRARY
             NAMES mysqlclient
             PATHS "$ENV{ProgramFiles}/MySQL/MySQL Server 8.0"
-                  "$ENV{ProgramFiles(x86)}/MySQL/MySQL Server 8.0"
+                  "$ENV{ProgramFiles\(x86\)}/MySQL/MySQL Server 8.0"
             PATH_SUFFIXES lib
         )
     endif()
